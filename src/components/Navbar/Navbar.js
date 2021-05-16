1d4c602087645f7ed34f3a7cfcbbe4799061ddaf
hithesh98 <kirrkirry@gmail.com>
2021-05-16 16:41:19 +0100

    add navbar menu icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
-import {MenuItems} from './MenuItems.js'
+import {MenuItems} from './MenuItems.js';
+import './Navbar.css'
 
 class Navbar extends Component {
+    state = {closebar:false}
+
+    handleMenuClick = () => {
+        this.setState({closebar: !this.state.closebar})
+    }
+
     render(){
         return(
             <nav className='navbar-items'>
                 <h1 className='navbar-title'>Byo</h1>
-                <div className='navbar-log'>
+                <div className='navbar-logo'>
 
+                </div>
+                <div className='menu-icon' onClick={this.handleMenuClick}>
+                    <i class= {this.state.closebar ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
                 <ul>
                     {MenuItems.map((item, index) => {
